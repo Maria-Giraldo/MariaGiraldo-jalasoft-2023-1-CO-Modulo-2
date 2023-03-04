dbcbed09caf01e915d14e50acd472619d2a5cec0
Maria-Giraldo <126831247+Maria-Giraldo@users.noreply.github.com>
2023-03-04 16:26:01 -0500

    Tarea-SecondClass

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,14 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 
 class Dinosaur(Sprite):
     X_POST = 80
     Y_POST = 310 
-    JUMP_SPEED = 8.5    
+    JUMP_SPEED = 8.5 
+    DUCK_SPEED = 8.5   
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -18,7 +19,8 @@ class Dinosaur(Sprite):
         self.dino_run = True
         self.dino_jump = False
         self.jump_speed = self.JUMP_SPEED
-
+        self.dino_duck = False
+        self.duck_speed = self.DUCK_SPEED
 
         
     def update(self, user_input):
@@ -26,12 +28,20 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()    
+
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+        if user_input[pygame.K_DOWN] and not self.dino_duck:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = False
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
-            self.dino_jump = False    
+            self.dino_jump = False 
+            self.dino_duck = False   
 
         if self.stop_index >=10:
             self.stop_index = 0
@@ -51,7 +61,18 @@ class Dinosaur(Sprite):
             self.dino_rect.y = self.Y_POST
             self.dino_jump = False
             self.jump_speed = self.JUMP_SPEED
+    def duck(self):
+        self.image = DUCKING[0] if self.stop_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POST
+        self.dino_rect.y = 350
+        self.stop_index += 1
+        self.duck_speed -=0.8
 
+        if self.duck_speed < -self.DUCK_SPEED:
+            self.dino_rect.y = self.Y_POST
+            self.dino_duck = False
+            self.duck_speed = self.DUCK_SPEED 
     def draw(self, screen):  
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y) )
 
